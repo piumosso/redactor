7eb0fcf808bc7f002435e9d69dabde20383e0835
piumosso <golikoffartem@gmail.com>
2013-08-21 23:24:54 +0400

    Using less for redactor stylesheets

--- playground.js
+++ playground.js
@@ -1,9 +1,11 @@
 var connect = require('connect'),
     open = require('open'),
+    lessMiddleware = require('less-middleware'),
     port = 8080;
 
 
 connect.createServer(
+    lessMiddleware({src: __dirname}),
     connect.static(__dirname)
 ).listen(port);
 
