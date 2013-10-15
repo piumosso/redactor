42b6bd0646c88538793c084890c2130aa57a595f
piumosso <golikoffartem@gmail.com>
2013-10-15 22:37:59 +0400

    Image block with plupload

--- playground.js
+++ playground.js
@@ -1,13 +1,23 @@
 var connect = require('connect'),
     open = require('open'),
     lessMiddleware = require('less-middleware'),
-    port = 8080;
+    port = 8080,
+    app;
 
 
-connect.createServer(
-    lessMiddleware({src: __dirname}),
-    connect.static(__dirname)
-).listen(port);
+connect.createServer()
+    .use(lessMiddleware({src: __dirname}))
+    .use(function(req, res, next){
+        if (req.url == '/playground/upload.json') {
+            res.end(JSON.stringify({
+                src: "example.jpg"
+            }));
+        } else {
+            next();
+        }
+    })
+    .use(connect.static(__dirname))
+    .listen(port);
 
 
 open('http://localhost:' + port + '/playground/index.html');
